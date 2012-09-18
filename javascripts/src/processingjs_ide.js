5fb7807827438aa24e54addb6ba76b177b6b7dda
Semmy Purewal <semmypurewal@gmail.com>
2012-09-17 21:14:29 -0400

    gets the code from the examples directory via ajax

--- javascripts/src/processingjs_ide.js
+++ javascripts/src/processingjs_ide.js
@@ -25,32 +25,19 @@ $(document).ready(function()  {
     ide.title("processing.js demo");
 
     //set up some default code
-    ide.code('// comments go here\n'+
-
-'void setup()\n'+
-'{\n'+
-'    size(200,200);\n'+
-'    background(125);\n'+
-'    fill(255);\n'+
-'    noLoop();\n'+
-'    PFont fontA = loadFont("courier");\n'+
-'    textFont(fontA, 14);\n'+
-'}\n\n'+
-'void draw(){\n'+
-'    text("Hello Web!",20,20);\n'+
-'    println("Hello ErrorLog!");\n'+
-'}');
-
+    $.get("examples/default.pjs", function (data) {
+        ide.code(data);
+    });
 
     // set the message box to fade in and fade out
     ide.messageOptions({
-	show:function(div)  {
-	    $(div).fadeIn();
-	}, 
-	hide:function(div)  {
-	    $(div).fadeOut();
-	},
-	time: 3000
+        show:function(div)  {
+            $(div).fadeIn();
+        }, 
+        hide:function(div)  {
+            $(div).fadeOut();
+        },
+        time: 3000
     });
 
     //RUN BUTTON CODE
@@ -58,57 +45,57 @@ $(document).ready(function()  {
     var error;  //processing error
 
     ide.button("run","images/icons/run.png", function()  {
-	return false;
+        return false;
     });
 
     ide.button("run").setAttribute("href","#canvas");
 
     $(ide.button("run")).fancybox({
-	'padding' : 0,
-	'titleShow' : false,
-	'type' : 'inline',
-	'width' : 'auto',
-	'height' : 'auto',
-	'scrolling' : 'no',
-	'hideOnOverlayClick' : 'true',
-	'transitionIn' : 'elastic',
-	'transitionOut' : 'elastic',
-	'onStart' : function()  {
-	    var code = ide.code();
-	    var canvas = document.getElementById("processing_canvas");
-	    error = null;
+        'padding' : 0,
+        'titleShow' : false,
+        'type' : 'inline',
+        'width' : 'auto',
+        'height' : 'auto',
+        'scrolling' : 'no',
+        'hideOnOverlayClick' : 'true',
+        'transitionIn' : 'elastic',
+        'transitionOut' : 'elastic',
+        'onStart' : function()  {
+            var code = ide.code();
+            var canvas = document.getElementById("processing_canvas");
+            error = null;
 
-	    try  {
-		p = new Processing(canvas, code);
-		var dimensions = code.match(/\s+size\((\d+),(\d+)\)/);
+            try  {
+                p = new Processing(canvas, code);
+                var dimensions = code.match(/\s+size\((\d+),(\d+)\)/);
                 var width = parseInt(dimensions[1]);
-		var height = parseInt(dimensions[2]);
+                var height = parseInt(dimensions[2]);
                 $("#processing_canvas").css('width',width);
                 $("#processing_canvas").css('height',height);
-	    }
-	    catch(err)  {
-		error = err;
-		Processing.logger.log(err);
-		$.fancybox.cancel();
-		if(p)  {
-		    p.exit();
-		}
-	    }
-	},
-	'onComplete' : function()  {
-	    if(error)  {
-		$.fancybox.close();
-	    } else  {
-		$("#fancybox-content").css('height',$("#processing_canvas").css('height'));                                                                                 
-		$("#fancybox-content").css('width',$("#processing_canvas").css('width'));
-		$("#processing_canvas").focus();
-	    }
-	},
-	'onCleanup' : function()  {
-	    if(p)  {
-		p.exit();
-	    }
-	}
+            }
+            catch(err)  {
+                error = err;
+                Processing.logger.log(err);
+                $.fancybox.cancel();
+                if(p)  {
+                    p.exit();
+                }
+            }
+        },
+        'onComplete' : function()  {
+            if(error)  {
+                $.fancybox.close();
+            } else  {
+                $("#fancybox-content").css('height',$("#processing_canvas").css('height'));                                                                                 
+                $("#fancybox-content").css('width',$("#processing_canvas").css('width'));
+                $("#processing_canvas").focus();
+            }
+        },
+        'onCleanup' : function()  {
+            if(p)  {
+                p.exit();
+            }
+        }
     });
     //END RUN BUTTON CODE
 });

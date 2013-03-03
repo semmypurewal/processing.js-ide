22dbc4defeff89a8bdacad2d4f0b968a861b401f
Semmy Purewal <semmypurewal@gmail.com>
2013-03-03 08:42:20 -0500

    fix bug introduced by previous commit

--- ide/javascripts/app/ide/views/ide.js
+++ ide/javascripts/app/ide/views/ide.js
@@ -210,9 +210,6 @@ window.jermaine.util.namespace("window.ide", function (ns) {
                         console.log(error);
                         $.colorbox.close();
                     } else  {
-                        if (p) {
-                            p.exit();
-                        }
                         $("#processing_canvas").focus();
                     }
                 },
